05181882650a34faf0ffe45c3016daf56f719af6
Moon602 <82143625+Moon602@users.noreply.github.com>
2021-11-11 23:55:02 +0900

    11/11 수정

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ console.log("Project ID :", process.env.PROJECT_ID)
 app.use(express.static(path.join(__dirname, "views")));
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const projectId = 'cookingchatbot'
 var context = 'projects/cookingchatbot/agent/sessions/5bd81f18-c941-4a8c-9e61-1b9be8c5ab20/contexts/cookingstep1-followup';
 var i=0
